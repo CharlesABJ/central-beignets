3eacace4234fe31f1f0fca0859390b95bda1cbf9
Charles ABJ <biadjeu.charles@orange.fr>
2025-04-13 12:37:57 +0200

    Code à jour

--- resources/js/app/header-menu-config.js
+++ resources/js/app/header-menu-config.js
@@ -6,15 +6,22 @@ document.addEventListener("DOMContentLoaded", () => {
   const html = document.querySelector("html");
   const menu = document.querySelector("header .menu-container");
   const logo = document.querySelector("header .logo");
+  const searchUserCart = document.querySelector("header .search-user-cart");
 
   toggleButton1.addEventListener("click", () => {
     menu.classList.add("menu-visible");
     html.classList.add("menu-visible");
     logo.style.color = "var(--menu-color)";
+    if (window.innerWidth < 678) {
+      searchUserCart.style.color = "var(--menu-color)";
+    }
   });
   toggleButton2.addEventListener("click", () => {
     menu.classList.remove("menu-visible");
     html.classList.remove("menu-visible");
     logo.style.color = "var(--black)";
+    if (window.innerWidth < 678) {
+      searchUserCart.style.color = "var(--black)";
+    }
   });
 });
